3ad44264c1d38eb249676f2949cd4a3a221eb43c
Matheus Ruzzi Ribeiro <14894087626@MUZAMBINHO.EDU.BR>
2026-05-06 10:45:36 -0300

    Primeiro commit

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,7 +4,10 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		return view('welcome_message');
+		
+		echo view('templates/header');
+		echo view('pages/home');
+		echo view('templates/footer');
 	}
 
 	//--------------------------------------------------------------------
---
+++ app/Views/templates/header.php
@@ -0,0 +1,1 @@
+<p>Cabesalho</p>
